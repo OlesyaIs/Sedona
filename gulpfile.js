1564bd815d67bcc8c246a13e9f882e71ef07072c
Olesia_Is <isichenko.ok@gmail.com>
2023-09-05 15:20:15 +0300

    For GithubPages (to deploy)

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ exports.styles = styles;
 
 const html = () => {
   return gulp.src("source/**/*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(htmlmin({ collapseWhitespace: false }))
     .pipe(gulp.dest("build"));
 }
 
